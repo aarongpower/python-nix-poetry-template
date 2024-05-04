531993b28159b32ab65a9bca9cf80ac366820d0d
Aaron Power <aarongpower@outlook.com>
2024-05-04 16:16:23 +0700

    flake.nix syntax

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
-outputs = { self }: {
-  templates.app = {
-    path = ./template;
-    description = "A python project using poetry2nix with direnv environment";
-  };
-  templates.default = self.templates.app;
+{
+  outputs = { self }: {
+    templates.app = {
+      path = ./template;
+      description = "A python project using poetry2nix with direnv environment";
+    };
+    templates.default = self.templates.app;
+  }
 }
